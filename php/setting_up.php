241f06194a5fc956e4cec0646c4d6642bb1c9282
Lisa Djerbi <ldjerbi@emi.u-bordeaux1.fr>
2014-02-13 15:58:34 +0100

    Adding database

---
+++ php/setting_up.php
@@ -0,0 +1,28 @@
+<?php
+
+
+define('CFG_DB_DSN', 'mysql://ldjerbi@dbserver/ldjerbi');
+
+require_once(dirname(__FILE__).'/../Doctrine-1.2.4/Doctrine.php');
+
+spl_autoload_register(array('Doctrine_Core', 'autoload'));
+
+
+$database = Doctrine_Manager::connection(CFG_DB_DSN)
+
+
+
+
+
+
+
+
+ $manager = Doctrine_Manager::getInstance();
+
+
+$manager->setAttribute(Doctrine_Core::ATTR_VALIDATE,               Doctrine_Core::VALIDATE_ALL);
+$manager->setAttribute(Doctrine_Core::ATTR_AUTO_ACCESSOR_OVERRIDE, true);
+$manager->setAttribute(Doctrine_Core::ATTR_AUTOLOAD_TABLE_CLASSES, true);
+$manager->setAttribute(Doctrine_Core::ATTR_MODEL_LOADING,          Doctrine_Core::MODEL_LOADING_CONSERVATIVE);
+//Doctrine_Core::loadModels(LIB_DIR.'models/');
+?>
