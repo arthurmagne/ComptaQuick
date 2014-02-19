4c8ab939a71e5ed375ecbaa3dce9b2218550b07b
arthurmagne <arthur.magne@hotmail.fr>
2014-02-19 14:54:43 +0100

    delete account from server + delete button on table [#14]

--- js/views/accountsTab.js
+++ js/views/accountsTab.js
@@ -5,29 +5,51 @@ define([
 	'underscore',
 	'backbone',
 	'text!../../templates/accountsTab.html',
-	'collections/accounts'
+	'collections/accounts',
+	'models/account'
 	], 
-	function(bootstrap, holder, $, _, Backbone, accountsTabTemplate, Accounts){
+	function(bootstrap, holder, $, _, Backbone, accountsTabTemplate, Accounts, Account){
 		var AccountsTab = Backbone.View.extend({
 			events: {
-
+				'click .clickableRow': 'detailAccount',
+				'click .delete-account': 'deleteAccount'
 			},
 
 			el: '#accountsTab',
 
 			render: function () {
 				console.log("Account tab view");
-				var accounts = new Accounts();
+				this.accounts = new Accounts();
 				var that = this;
 
-		        accounts.fetch({
+		        this.accounts.fetch({
 		        	success: function (accounts) {
 		        		console.log("accounts fetch success");
 		        		var template = _.template(accountsTabTemplate, {accounts: accounts.models});
 		        		that.$el.html(template);
 		        	}
 		        });
-    }
+
+    		},
+
+    		detailAccount: function (event) {
+    			console.log($(event.currentTarget).attr("href"));
+
+    		},
+
+    		deleteAccount: function (event) {
+    			event.stopImmediatePropagation();
+    			var accountId = $(event.currentTarget).data('value');
+    			console.log(accountId);
+    			console.log("Delete account with id : ", accountId);
+    			console.log(this.accounts.get(accountId));
+    			// remove model (from server and collection by bubbling)
+
+    			this.accounts.get(accountId).destroy();
+
+  
+
+    		}
 });
 
   // Our module now returns our view
